a1ec36af79f7d1f9f01893178558ec19c0411f6f
tvm-gopika <gopika.vipin@thrithvam.ae>
2015-05-13 12:55:20 +0530

    Changes made

---
+++ FlyCon1.0/Models/trkPunchListModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlyCon1._0.Models
+{
+    public class trkPunchListModel
+    {
+        public List<trkPunchListModel> PunchListCollection
+        {
+            get;
+            set;
+        }
+        public string ProjNo
+        {
+            get;
+            set;
+        }
+        public int IdNo
+        {
+            get;    
+            set;
+        } 
+        public int LinkId
+        {
+            get;
+            set;
+        }
+        public string PunchType
+        {
+            get;
+            set;
+        }
+        public string OpenBy
+        {
+            get;
+            set;
+        }
+        public DateTime OpenDt
+        {
+            get;
+            set;
+        }
+        public string EnteredBy
+        {
+            get;
+            set;
+        }
+        public DateTime EnteredDt
+        {
+            get;
+            set;
+        }
+        public string Plant
+        {
+            get;
+            set;
+        }
+        public string Area
+        {
+            get;
+            set;
+        }
+        public string Location
+        {
+            get;
+            set;
+        }
+        public string Unit
+        {
+            get;
+            set;
+        }
+
+    }
+   
+    }
